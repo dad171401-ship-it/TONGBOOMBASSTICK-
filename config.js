9ffb7f458b574e92907feda5e366ed541bd9a6dd
dad171401-ship-it <dad171401@gmail.com>
2025-12-01 12:55:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb srv//shivashival@musicbotyt.ouljyww.mongodb.net//retryWrites truecor majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
